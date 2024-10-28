997fccd23ce6320785b673729b55bb2f5475033d
enricomilli <enricomilli666@gmail.com>
2024-10-28 10:11:31 +0000

    feat: kruskal's minimun spanning tree

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from adjacency_list_graph import AdjacencyListGraph
 from constants import UNDERGROUND_DATA_FILE
 from read_file import get_underground_data
 from utils import format_underground_data, get_all_stations, get_all_connections
+from mst import kruskal
 
 underground_data = get_underground_data(UNDERGROUND_DATA_FILE)
 
@@ -10,7 +11,7 @@ formated_data = format_underground_data(underground_data)
 all_stations = get_all_stations(formated_data)
 all_connections = get_all_connections(formated_data)
 
-underground_graph = AdjacencyListGraph(len(all_stations), True, True)
+underground_graph = AdjacencyListGraph(len(all_stations), False, True)
 
 for edge in all_connections:
     try:
@@ -22,4 +23,7 @@ for edge in all_connections:
         print('already added this edge:', edge)
         pass
 
-print('program finished')
+
+# Kruskals algorithm
+minimum_spanning_tree = kruskal(underground_graph)
+print(minimum_spanning_tree)
