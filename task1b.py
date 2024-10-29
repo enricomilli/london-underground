2a20569a53d7ef14688c474ee2a9475bc18bf77b
enricomilli <enricomilli666@gmail.com>
2024-10-29 08:08:04 +0000

    fix: using range function for task1b

--- task1b.py
+++ task1b.py
@@ -1,6 +1,5 @@
 from generate_random_graph import generate_random_graph
 
-for i in range(10):
-    num_of_nodes = (i+1) * 100
+for num_of_vertices in range(100, 1100, 100):
 
-    graph = generate_random_graph(num_of_nodes, 0.5, True, False, True, 0, 10)
+    graph = generate_random_graph(num_of_vertices, 0.5, True, False, True, 0, 10)
